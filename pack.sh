7274d3235c0668a0311b101000dfab0f45dc3637
Haikulet <haikulet@outlook.com>
2026-06-21 23:18:57 +0800

    feat: add quick pack zip sh script

---
+++ pack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# 获取脚本所在目录并进入
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+mkdir words_plugin
+cp -r qml/ words_plugin/
+cp icon.png words_plugin/
+cp build/linux/arm64-v8a/release/libwords_plugin.so words_plugin/
+cp metadata.json words_plugin/
+cp -r dict/ words_plugin/
+zip -r words_plugin.zip words_plugin/
+echo "打包完成"
