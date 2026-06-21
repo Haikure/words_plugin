a1295c9d8ada962a4de4e5dc53847235fea67eb7
Haikulet <haikulet@outlook.com>
2026-06-21 23:19:41 +0800

    v1.0.1

--- pack.sh
+++ pack.sh
@@ -9,6 +9,7 @@ cp -r qml/ words_plugin/
 cp icon.png words_plugin/
 cp build/linux/arm64-v8a/release/libwords_plugin.so words_plugin/
 cp metadata.json words_plugin/
-cp -r dict/ words_plugin/
+cp -r dicts/ words_plugin/
 zip -r words_plugin.zip words_plugin/
+rm -r words_plugin/
 echo "打包完成"
